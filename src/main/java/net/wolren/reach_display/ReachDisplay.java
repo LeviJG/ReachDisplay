a4d9a02e8952bdbdab1a7422742376ccbb6958de
Levi <levi@gbros.net>
2025-06-13 12:24:00 -0500

    Minor code updates

--- src/main/java/net/wolren/reach_display/ReachDisplay.java
+++ src/main/java/net/wolren/reach_display/ReachDisplay.java
@@ -3,8 +3,6 @@ package net.wolren.reach_display;
 import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ModInitializer;
 import net.wolren.reach_display.config.DisplayConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ReachDisplay implements ModInitializer {
     public static final String MOD_ID = "reach_display";
